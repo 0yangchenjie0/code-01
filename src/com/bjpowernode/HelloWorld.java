26b9f2c76015aa5cb859e273782ed3d3cd146026
jack <jack@126.com>
2023-04-18 16:35:12 +0800

    测试

--- src/com/bjpowernode/HelloWorld.java
+++ src/com/bjpowernode/HelloWorld.java
@@ -2,6 +2,6 @@ package com.bjpowernode;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello Git Git");
     }
 }
